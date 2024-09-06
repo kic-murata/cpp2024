2b4a69d01962ca9b20927d99d79a4ed639ba6107
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2024-09-06 15:00:20 +0900

    20240906

---
+++ Sample301c_1/car.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Car {
+public:
+	void drive(double hour);
+	void setSpeed(double speed);
+private:
+	double m_speed;
+};
---
+++ Sample304_1/sample.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Sample {
+public:
+	void setNum(int num);
+	int getNum();
+private:
+	int m_num;
+};
