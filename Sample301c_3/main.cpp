c530af61652e44baa18757383a231419b05930c8
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2024-09-11 12:11:52 +0900

    20240911

---
+++ Sample301c_3/main.cpp
@@ -0,0 +1,9 @@
+#include "car.h"
+
+int main()
+{
+	Car kuruma;
+	//kuruma.setSpeed(40);
+	kuruma.drive(1.5);
+	return 0;
+}
---
+++ Sample402_3/main.cpp
@@ -0,0 +1,18 @@
+#include "car.h"
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//Car* pKuruma = nullptr;
+	//pKuruma = new Car();
+	unique_ptr<Car> pKuruma(new Car());
+	pKuruma->setSpeed(40);
+	pKuruma->drive(1.5);
+	pKuruma->drive(2.0);
+	cout << "総移動距離:" <<
+		pKuruma->getMigration() << "km" << endl;
+	//delete pKuruma;
+	cout << "インスタンスの消去終了" << endl;
+	return 0;
+}
