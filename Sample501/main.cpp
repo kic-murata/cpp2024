961a605511436ad9178b05b120a9e191b645097d
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2024-09-12 12:14:46 +0900

    20240912

--- Sample501/main.cpp
+++ Sample501/main.cpp
@@ -7,17 +7,19 @@ using namespace std;
 int main()
 {
   cout << "Carクラスの処理" << endl;
-  Car* pkuruma = new Car();
+  //Car* pkuruma = new Car();
+  unique_ptr<Car> pkuruma(new Car());
   pkuruma->setSpeed(40);
   pkuruma->drive(1.5);
   cout << "総移動距離:" << pkuruma->getMigration() << "km" << endl;
-  delete pkuruma;
+  //delete pkuruma;
   cout << "Ambulanceクラスの処理" << endl;
-  Ambulance* pAmb = new Ambulance();
+  unique_ptr<Ambulance> pAmb(new Ambulance());
+  //Ambulance* pAmb = new Ambulance();
   pAmb->setSpeed(60);
   pAmb->drive(2);
   cout << "総移動距離:" << pAmb->getMigration() << "km" << endl;
   pAmb->savePeople();
-  delete pAmb;
+  //delete pAmb;
   return 0;
 }
