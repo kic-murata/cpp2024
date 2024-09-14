f56d3333bb4556b3c4df340bdcf9577b81f03184
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2024-09-14 17:19:32 +0900

    20240914

--- Sample503/calc.h
+++ Sample503/calc.h
@@ -1,11 +1,10 @@
 #pragma once
-
 class Calc {
 private:
 	int m_a, m_b;
 public:
 	Calc();
-	Calc(int a, inb b);
+	Calc(int a, int b);
 	int add();
 	int add(int a, int b);
 	void setValue(int a, int b);
