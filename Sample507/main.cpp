5329108781e9ee57ea513d2083e235c4cb4a5919
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2024-09-19 14:52:41 +0900

    20240919

--- Sample507/main.cpp
+++ Sample507/main.cpp
@@ -6,21 +6,19 @@ using namespace std;
 
 int main()
 {
-	////Crow* pCrow(new Crow());
-	////Chicken* pChicken(new Chicken());
-	//Bird* pCrow(nullptr), * pChicken(nullptr);
-	//pCrow = new Crow;
- // pChicken = new Chicken;
-	unique_ptr<Bird> pCrow = make_unique<Crow>();
-	unique_ptr<Bird> pChicken = make_unique<Chicken>();
+	Bird* pCrow{}, * pChicken{}, * pBird{};
+	pCrow = new Crow();
+  pChicken = new Chicken();
+	pBird = new Bird();
+	//unique_ptr<Bird> pCrow = make_unique<Crow>();
+	//unique_ptr<Bird> pChicken = make_unique<Chicken>();
 	//unique_ptr<Bird> pCrow(new Crow());
 	//unique_ptr<Bird> pChicken(new Chicken());
-	Bird* pBird = new Bird();
 	pCrow->fly();
 	pChicken->fly();
 	pCrow->sing();
 	pChicken->sing();
-	//delete pCrow;
-	//delete pChicken;
+	delete pCrow;
+	delete pChicken;
 	return 0;
 }
