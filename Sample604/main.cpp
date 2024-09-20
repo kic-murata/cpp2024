73cf32aee36b9955517b18593c03b7b5fe5024fb
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2024-09-20 13:02:41 +0900

    20240920

--- Sample604/main.cpp
+++ Sample604/main.cpp
@@ -6,16 +6,33 @@ using namespace std;
 int main()
 {
 	vector<int> intVec;
+	vector<int> intVec2{ 99,98,97 };
 	vector<string> strVec;
+	if (intVec.empty()) {
+		cout << "intVec‚Í‹ó‚Ì”z—ñ" << endl;
+	}
 	intVec.push_back(1);
 	intVec.push_back(2);
 	intVec.push_back(3);
+	intVec2.emplace(intVec2.begin()+2,100);
 	strVec.push_back("ABC");
 	strVec.push_back("DEF");
-	for (const auto& v : strVec) {
-		cout << v << endl;
+	for (const auto& v : intVec) {
+		cout << v << " " << endl;
 	}
+	cout << "intVec1.size=" << intVec.size() << endl;
+	intVec.insert(intVec.begin()+1, 11);
+	cout << "intVec1.size=" << intVec.size() << endl;
+	for (const auto& v : intVec2) {
+		cout << v << " " << endl;
+	}
+	cout << "intVec2.size=" << intVec2.size() << endl;
+	cout << "intVec1.size=" << intVec.size() << endl;
 	intVec.pop_back();
+	cout << "intVec1.size=" << intVec.size() << endl;
+	for (const auto& v : intVec) {
+		cout << v << " " << endl;
+	}
 	//for (auto i = intVec.begin(); i < intVec.end(); i++) {
 	//	cout << "intVec[" << i << "]=" << intVec[i] << endl;
 	//}
