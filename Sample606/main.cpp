d6e1b7e3a8490a4f316addb769c9eb54e534f818
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2024-09-30 17:12:26 +0900

    20240930

---
+++ Sample606/main.cpp
@@ -0,0 +1,29 @@
+#include <list>
+#include <iostream>
+using namespace std;
+
+int main() {
+	list<int> li{};
+	li.push_back(1);
+	li.push_back(2);
+	li.emplace_front(3);
+	cout << "size:" << li.size() << endl;
+	auto itr = li.begin();
+	itr++;
+	li.insert(itr, 4);
+	cout << "size:" << li.size() << endl;
+	//for (itr = li.begin(); itr != li.end(); itr++) {
+	for(auto itr: li){
+		cout << itr << " ";
+	}
+	cout << endl;
+	li.erase(li.begin());
+	cout << "size:" << li.size() << endl;
+	li.remove(4);
+	cout << "size:" << li.size() << endl;
+	for (auto itr : li) {
+		cout << itr << " ";
+	}
+	cout << endl;
+	return 0;
+}
