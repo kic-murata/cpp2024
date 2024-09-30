640568e6c9352af0179ddecaa000779f39b768ed
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2024-09-30 14:54:21 +0900

    20240930

--- Sample604/main.cpp
+++ Sample604/main.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <array>
 using namespace std;
 int main()
 {
+	array<int,3> arr{ 1,2,3 };
 	vector<int> intVec;
 	vector<int> intVec2{ 99,98,97 };
 	vector<string> strVec;
+	cout << arr[0] << endl;
+	cout << arr.at(1) << endl;
+	//arr.swap(0, 1);
+	cout << arr.front() << endl;
+	cout << arr.back() << endl;
+	arr.fill(100);
+	//cout << arr.fill(100) << endl;
+	cout << arr.at(1) << endl;
 	if (intVec.empty()) {
 		cout << "intVecは空の配列" << endl;
+		cout << "capa:" << intVec.capacity() << endl;
 	}
 	intVec.push_back(1);
 	intVec.push_back(2);
 	intVec.push_back(3);
+	cout << "capa:" << intVec.capacity() << endl;
 	intVec2.emplace(intVec2.begin()+2,100);
 	strVec.push_back("ABC");
 	strVec.push_back("DEF");
@@ -43,13 +55,16 @@ int main()
 	intVec.insert(itrInt + 1, 111);
 	intVec.erase(itrInt + 2);
 	for (int i = 0; i < intVec.size(); i++) {
-		cout << "intVec[" << i << "]=" << intVec[i] << endl;
+	//	cout << "intVec[" << i << "]=" << intVec[i] << endl;
+		cout << "intVec[" << i << "]=" << intVec.at(i) << endl;
 	}
-	for (auto itrInt = intVec.begin(); itrInt != intVec.end(); ++itrInt) {
+	cout << "イテレータを使った表示" << endl;
+	for (auto itrInt = intVec.begin()+1; itrInt != intVec.end(); itrInt++) {
 		cout << *itrInt << endl;
 	}
+	cout << "範囲for文を使った表示" << endl;
 	for (auto itr : intVec) {
-		cout << *itrInt << endl;
+		cout << itr << endl;
 	}
 	for (int i = 0; i < strVec.size(); i++) {
 		cout << "strVec[" << i << "]=" << strVec[i] << endl;
--- SampleRPG/main.cpp
+++ SampleRPG/main.cpp
@@ -1,13 +1,39 @@
 #include "chara.h"
 #include "player.h"
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-	Player* pPlayer = new Player(100, 50, 20, 30);
-	cout << "Playerの状態 HP:" << pPlayer->getHp()
-		<< " SP:" << pPlayer->getSp()
-		<< " Atk:" << pPlayer->getAtk()
-		<< " Def:" << pPlayer->getDef() << endl;
+	//Player* pPlayer = new Player(100, 50, 20, 30);
+	vector<Player*> pPlayer{ new Player(100, 50, 20, 30) };
+	pPlayer.push_back(new Player(300, 70, 40, 50));
+	//cout << "Playerの状態 HP:" << pPlayer->getHp()
+	//	<< " SP:" << pPlayer->getSp()
+	//	<< " Atk:" << pPlayer->getAtk()
+	//	<< " Def:" << pPlayer->getDef() << endl;
+	for (int i = 0; i < pPlayer.size(); i++) {
+		cout << "adress:" << pPlayer[i] << endl;
+		cout << "Playerの状態" << endl
+			<< " HP: " << pPlayer.at(i)->getHp() << endl
+			<< " SP: " << pPlayer[i]->getSp() << endl
+			<< " Atk:" << pPlayer[i]->getAtk() << endl
+			<< " Def:" << pPlayer[i]->getDef() << endl;
+	}
+	auto itr = pPlayer.begin();
+	while ( itr != pPlayer.end() ) {
+		cout << "インスタンス削除" << *itr << endl;
+		delete *itr;
+		//itr++;
+		cout << "配列削除" << endl;
+		itr = pPlayer.erase(itr);
+	}
+	for (auto it : pPlayer) {
+		cout << it << endl;
+	}
+	if (pPlayer.empty()) {
+		cout << "empty!" << endl;
+	}
+	cout << endl << "size:" << pPlayer.size() << endl;
 	return 0;
 }
