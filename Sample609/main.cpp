882e7675887ec201b1b6da543ffdf8804e7ada22
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2024-10-02 12:59:51 +0900

    20241002

--- Sample609/main.cpp
+++ Sample609/main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 #include <set>
 #include <string>
+
+class Chara {
+public:
+	int a_;
+private:
+	int b_;
+};
+
 using namespace std;
 int main() {
+	Chara c;
+	c.a_ = 10;
+	cout << "a=" << c.a_ << endl;
 	set<string> names{ "John", "Sam"}; //初期値の設定
 	names.insert("Tom");
 	names.insert("Mike");
 	names.insert("Mike"); //重複キーの登録
 	names.insert("Bob");
 	names.erase("Sam");  //キーの削除
-	for (auto it = names.begin(); it != names.end(); it++) {
+	/*for (auto it = names.begin(); it != names.end(); it++) {
 		cout << *it << endl;
-	}
+	}*/
 	for (auto it : names) { //範囲for
 		cout << it << endl;
 	}
