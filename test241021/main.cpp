c13dbfd880548f0df2814a45b0efef0b7a77cd13
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2024-10-21 16:43:18 +0900

    20241021

---
+++ test241021/main.cpp
@@ -0,0 +1,28 @@
+/*
+vectorに構造体を格納して、特定のメンバの値を使ってソートを行うサンプル
+*/
+
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include<tuple>
+using namespace std;
+
+typedef struct {
+	int a, b, c;
+} Item;
+
+struct CompItem { // 大小比較用の関数オブジェクトを定義する
+	bool operator()(const Item& item1, const Item& item2) const noexcept {
+		// キーとして比較したい要素を列挙する(今回はcメンバのみの大小比較)
+		return std::tie(item1.c) < std::tie(item2.c);
+	}
+};
+
+int main() {
+	vector<Item> vec{ {11,9,8},{8,10,12},{20,5,1},{10,2,3} };
+	sort(vec.begin(), vec.end(), CompItem{});
+	for (Item it : vec) {
+		cout << it.a << " " << it.b << " " << it.c << endl;
+	}
+}
