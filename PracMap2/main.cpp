ce8201ab54899edcd900d024991362cfb918d341
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2024-10-24 12:51:15 +0900

    20241024

--- PracMap2/main.cpp
+++ PracMap2/main.cpp
@@ -7,11 +7,21 @@ int main() {
 		{"雰囲気","atmosphere"},{"血液","blood"},
 		{"環境","environment"},{"温度","temperature"}};
 
-	random_device rand_dev{};
-	mt19937 rand_engine(rand_dev());
-	uniform_int_distribution<int> dist(0, dic.size() - 1);  // 1～6 を均等な確率で得る分布生成器
+	cout << dic.begin()->first << endl;
+//	for (auto p : dic) {
+	for(auto it = dic.begin(); it != dic.end(); it++){
+		cout << "キー：" << it->first << endl;
+	}
 
+
+	//乱数生成
+	random_device rand_dev{}; //乱数生成器を作成
+	mt19937 rand_engine(rand_dev());//アルゴリズムにメルセンヌツイスターを使う
+	uniform_int_distribution<int> dist(0, dic.size() - 1);  // 0～mapの要素数-1 を均等な確率で得る分布生成器
+
+	//mapの先頭のイテレータ取得
 	auto it = dic.begin();
+	//乱数とnext関数を使ってイテレータを更新
 	it = next(it, dist(rand_engine));
 	cout << it->first << "を英語に直すと？";
 	string input;
@@ -22,4 +32,15 @@ int main() {
 	else {
 		cout << "不正解" << endl;
 	}
+
+	map<int, map<string, int>> 
+		tMap{ { 1, { {"abc", 3} } }, { 2, { {"abc", 1} } },
+					{ 3, { {"def", 9} } }, { 4, { {"def", 7} } }
+	};
+	cout << tMap[1]["abc"] << endl;
+	cout << tMap[2]["abc"] << endl;
+	cout << tMap[3]["def"] << endl;
+	cout << tMap[4]["def"] << endl;
+
+	return 0;
 }
