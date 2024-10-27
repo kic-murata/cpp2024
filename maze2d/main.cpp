d170e349d5ea4b0c797a093a19c757148cc3692d
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2024-10-28 00:25:53 +0900

    20241027

--- maze2d/main.cpp
+++ maze2d/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <conio.h>
-#include <cstdlib>
-#include <ctime>
+//#include <cstdlib>
+//#include <ctime>
 #include <Windows.h>
 #include <vector>
 #include <fstream>
@@ -9,10 +9,13 @@
 #include <random>
 #include <algorithm>
 
-//using namespace std;
+using namespace std;
 
-std::vector<std::vector<int>> mapdata(22, std::vector<int>(22));
-static std::string m_fileneme { "2d_maze22.csv" };
+//22行×22列の二次元配列用領域を確保
+vector<vector<int>> mapdata{};
+//vector<vector<int>> mapdata(22, vector<int>(22));
+//マップデータファイル名
+static string m_fileneme { "2d_maze22.csv" };
 
 constexpr int WIDTH = 22;     //マップの横幅
 constexpr int HEIGHT = 22;    //マップの縦幅
@@ -25,39 +28,57 @@ char map[HEIGHT][WIDTH];    //WIDTH×HEIGHTのマップ作成(文字型配列)
 int playerX, playerY;       //プレイヤーのX、Y座標
 int treasureX, treasureY;   //宝箱のX,Y座標
 
+class Player {
+private:
+    int x, y;
+};
+
+class Treasure {
+private:
+    int x, y;
+};
+
 // 文字列と区切り文字','から、CSVファイルの値を分離する関数
-std::vector<std::string> split(std::string& input, char delimiter)
+vector<string> split(string& input, char delimiter)
 {
-    std::istringstream stream(input);
-    std::string field;
-    std::vector<std::string> result;
-    while(std::getline(stream, field, delimiter)){
+    istringstream stream(input);
+    string field;                   // 一行分まるまる読み込んだデータを格納する文字列
+    vector<string> result;          // カンマで区切った項目を格納する配列
+    while(getline(stream, field, delimiter)){
         result.push_back(field);    // 一行分の配列要素（22個）を格納したvectorを作る
     }
     // 読み取って分解した要素数を表示
-    // std::cout << result.size() << std::endl;
+    // cout << result.size() << endl;
     return result;
 }
 
 void MapdataRead(){
+
    // ファイルストリームを作成して、2d_maze.csvを開く
-    std::ifstream ifs(m_fileneme);
+    ifstream ifs(m_fileneme);
     // ファイルオープンエラーチェック
     if(ifs.fail()){
-        std::cout << "file open error" << std::endl;
+        cout << "file open error" << endl;
     } else {
-        std::cout << "file open success" << std::endl;
+        cout << "file open success" << endl;
     }
     // ファイルから一行ずつ読み込むためのstring型変数lineを宣言
-    std::string linetext;
+    string linetext;
     int j = 0;
     // ファイルの終わりまで一行ずつ読み込んでいく
-    while(std::getline(ifs, linetext)){
+    while(getline(ifs, linetext)){
         // 読み込んだ一行をsplit関数で分割して、vector型のstrvecへ格納する
-        std::vector<std::string> strvec = split(linetext, ',');
+        vector<string> strvec = split(linetext, ',');
+        cout << "strvec.size:" << strvec.size() << endl;
+        cout << "mapdata.size:" << mapdata.size() << endl;
+        mapdata.resize(mapdata.size() + 1);
+//        mapdata.emplace_back(strvec);
+//        mapdata.push_back(strvec);
+        cout << "mapdata.size:" << mapdata.size() << endl;
         for(int i = 0; i < strvec.size(); i++){
-            // std::cout << stoi(strvec.at(i));
-            mapdata[j][i] = stoi(strvec.at(i));
+            // cout << stoi(strvec.at(i));
+            //mapdata[j][i] = stoi(strvec.at(i));
+            mapdata.push_back(stoi(strvec.at(i)));
         }
         j++;
     }
@@ -84,13 +105,13 @@ void InitializeMap()        //マップ内を「.」で埋める
 void GenerateTreasure()     //乱数で宝箱の位置を決定
 {
   // 乱数生成器のオブジェクトrnd_devを作成
-  std::random_device rnd_dev{};
+  random_device rnd_dev{};
   // 疑似乱数（メルセンヌツイスター）のために初期シード（乱数）を与える
-  std::mt19937 rand_engine(rnd_dev());
+  mt19937 rand_engine(rnd_dev());
   // X座標用の範囲を指定した分布生成器オブジェクトrnd_xを生成
-  std::uniform_int_distribution<int> rnd_x(2, WIDTH-3);
+  uniform_int_distribution<int> rnd_x(2, WIDTH-3);
   // Y座標用の範囲を指定した分布生成器オブジェクトrnd_yを生成
-  std::uniform_int_distribution<int> rnd_y(2, HEIGHT-3);
+  uniform_int_distribution<int> rnd_y(2, HEIGHT-3);
   //指定範囲（2～47の範囲）で乱数を生成する
   treasureX = rnd_x(rand_engine);
   treasureY = rnd_y(rand_engine);
@@ -102,36 +123,29 @@ void GenerateTreasure()     //乱数で宝箱の位置を決定
 
 void DrawMap()              //マップデータの表示
 { 
+    //マップはプレイヤーの位置を中心として、5×5マスぶんだけ画面に表示
+    // #####
+    // # ###
+    // # @
+    // # ###
+    // # ###
+    //そのため、プレイヤーの位置からX座標、Y座標ともに２マスぶん小さい
+    //ところから、２マスぶん大きい座標までの間のデータを画面に表示する
     for (int y = playerY-2; y <= playerY+2; y++)
     {
         for (int x = playerX-2; x <= playerX+2; x++)
         {
             if (x == playerX && y == playerY)   //プレイヤーのいる座標に
             {
-                std::cout << PLAYER;                 //プレイヤーの文字を表示
+                cout << PLAYER;                 //プレイヤーの文字を表示
             }
             else
             {
-                std::cout << map[y][x];              //プレイヤー以外の場所を表示
+                cout << map[y][x];              //プレイヤー以外の場所を表示
             }
         }
-        std::cout << std::endl;                           //一行ぶん表示した後改行
+        cout << endl;                           //一行ぶん表示した後改行
     }
-    // for (int y = 0; y < HEIGHT; y++)
-    // {
-    //     for (int x = 0; x < WIDTH; x++)
-    //     {
-    //         if (x == playerX && y == playerY)   //プレイヤーのいる座標に
-    //         {
-    //             cout << PLAYER;                 //プレイヤーの文字を表示
-    //         }
-    //         else
-    //         {
-    //             cout << map[y][x];              //プレイヤー以外の場所を表示
-    //         }
-    //     }
-    //     cout << endl;                           //一行ぶん表示した後改行
-    // }
 }
 
 void MovePlayer(char direction)                 //入力された文字によるプレイヤーの移動処理
@@ -153,6 +167,8 @@ void MovePlayer(char direction)                 //入力された文字によるプレイヤー
         case 'd': // 右
             newPlayerX++;
             break;
+        case '@':
+            exit(0);
     }
 
     if (newPlayerX >= 0 && newPlayerX < WIDTH && newPlayerY >= 0 && newPlayerY < HEIGHT) //移動範囲チェック
@@ -161,10 +177,9 @@ void MovePlayer(char direction)                 //入力された文字によるプレイヤー
             playerX = newPlayerX;   //移動可能ならプレイヤー位置を更新
             playerY = newPlayerY;
 
-//            if (playerX == treasureX && playerY == treasureY)   //プレイヤーの位置が宝箱のとき
             if (map[playerY][playerX] == TREASURE)   //プレイヤーの位置が宝箱のとき
             {
-                std::cout << "\033[31mお宝を見つけました！ゲームクリアです。\033[m" << std::endl;
+                cout << "\033[31mお宝を見つけました！ゲームクリアです。\033[m" << endl;
                 exit(0);
             }
 
@@ -174,20 +189,18 @@ void MovePlayer(char direction)                 //入力された文字によるプレイヤー
 
 int main()
 {
-    
     //srand(time(NULL));  //乱数初期化
 
     MapdataRead();      //CSVからのデータ読み込み
     InitializeMap();    //マップ初期化
     GenerateTreasure(); //宝箱生成
 
-    playerX = 2;    //WIDTH / 2;    //最初の出現位置の設定（マップの真ん中）
-    playerY = 2;    //HEIGHT / 2;
+    playerX = 2;    //プレイヤーの出現位置の設定
+    playerY = 2;    //(X,Y) = (2,2)
 
-    system("cls");
+    //system("cls");  //コマンドプロンプトの画面消去
     while (true)
     {
-        //system("cls");
         // 標準出力画面のカーソル位置を(0,0)へ設定する
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD { 0, 0 });
         DrawMap();
