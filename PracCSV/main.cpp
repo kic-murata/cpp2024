15fd381a95c471cadd58703de1eb9f465c2475cd
Murata-KIC <murata@st.kobedenshi.ac.jp>
2024-11-08 17:29:21 +0900

    20241108

--- PracCSV/main.cpp
+++ PracCSV/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 using namespace std;
 int main() {
@@ -10,9 +11,13 @@ int main() {
 		return -1;
 	}
 	string text;
-	getline(ifs, text);
-	while (getline(ifs, text)) {
-		cout << text << endl;
+	getline(ifs, text);//先頭行を読み飛ばし
+	while (getline(ifs, text)) {//ファイル末尾まで一行ずつ読み込む
+		istringstream iss(text);//文字列ストリームに変換
+		while(getline(iss, text, ',')) {//文字列ストリームを,で分割
+			cout << text << " ";
+		}
+		cout << endl;
 	}
 	ifs.close();
 	return 0;
