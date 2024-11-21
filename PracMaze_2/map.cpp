2c1ea753192d57eb9f1a89c5aca77bdbf19364fd
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2024-11-21 12:12:05 +0900

    20241121

---
+++ PracMaze_2/map.cpp
@@ -0,0 +1,39 @@
+#include "map.h"
+#include "maze.h"
+#include <fstream>
+#include <sstream>
+#include <random>
+#include <algorithm>
+using namespace std;
+Map::Map() = default;											//デフォルトコンストラクタ
+void Map::Load(string filename) {					//CSVファイルの読み込み
+	ifstream ifs(filename);									//入力ファイルストリームの生成
+	if (ifs.fail()) {												//エラーが発生したときの処理
+		cout << "ファイルオープンエラー" << endl;	//エラーメッセージの表示
+		exit(-1);															//エラーコード-1で終了
+	}
+	string text;														//文字列型変数
+	int j = 0;															//二次元配列の行数を管理する変数
+	while (getline(ifs, text)) {						//CSVから一行ずつ読み込む
+		mapdata.resizeMap();									//二次元配列に行を追加
+		istringstream iss(text);							//文字列ストリームを生成
+		while (getline(iss, text, ',')) {			//ストリームをコンマで分割
+			mapdata.addMap(j, stoi(text));			//分割された項目を配列に格納
+		}
+		j++;																	//行数をカウントアップ
+	}
+	ifs.close();														//ファイルを閉じる
+}
+void Map::DrawMap() {
+	for (int y = 0; y < mapdata.getMapSize(); y++) {
+		for (int x = 0; x < mapdata.getMapSize(y); x++) {
+			if (mapdata.getMapValue(x, y) == WALL) {
+				cout << "#";
+			}
+			else if (mapdata.getMapValue(x, y) == ROAD) {
+				cout << ".";
+			}
+		}
+		cout << endl;
+	}
+}
--- PracMaze_2/map.h
+++ PracMaze_2/map.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "maze.h"
+#include <iostream>
 
 class Map {
 public:
